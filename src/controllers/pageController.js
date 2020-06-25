2538b61f67db766593f2824ffdaf45ebca85f4fa
zhenhua <nkzhenhua@gmail.com>
2020-06-24 22:17:30 -0700

    download web page with node-website-scraper

--- src/controllers/pageController.js
+++ src/controllers/pageController.js
@@ -1,3 +1,6 @@
+const scrape = require('website-scraper');
+const fs = require('fs-extra');
+const sha1 = require('sha1');
 
 /**
  * Download a web page link and email to kindle address
@@ -5,6 +8,27 @@
  * @param {*} res 
  */
 async function sendLinkToKindle(req, res) {
+
+    var urls = [];
+    urls.push(req.body.url);
+
+    var directory = sha1(req.body.url);
+
+    const options = {
+        urls,
+        directory,
+        sources: [
+            { selector: 'img', attr: 'src' }
+        ],
+        ignoreErrors: true
+    };
+    try{
+        await scrape(options);
+        //await fs.remove();
+    }catch(err){
+        console.log(err);
+        res.send("error when download page")
+    }
     res.send(req.body);
 };
 
